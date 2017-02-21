0a476d34fdad3b07b368fd077e3d4a93bd1e0ae1
wyu68 <wyu68@gatech.edu>
2017-02-21 14:30:30 -0500

    add UPOSI support

--- gym/envs/dart/hopper.py
+++ gym/envs/dart/hopper.py
@@ -30,10 +30,24 @@ class DartHopperEnv(dart_env.DartEnv, utils.EzPickle):
         if self.train_UP:
             obs_dim += 1
         self.param_manager = hopperContactManager(self)
+
+        # UPOSI variables
+        self.use_UPOSI = False
+        self.history_length = 3 # size of the motion history for UPOSI
+        self.state_action_buffer = []
+
         dart_env.DartEnv.__init__(self, 'hopper.skel', 4, obs_dim, self.control_bounds)
+
         utils.EzPickle.__init__(self)
 
+    def setUseUPOSI(self, useUPOSI = True):
+        self.use_UPOSI = useUPOSI
+        self.OSI_obs_dim = (self.obs_dim+self.act_dim)*self.history_length+self.obs_dim
+
     def _step(self, a):
+        if self.use_UPOSI:
+            self.state_action_buffer[-1].append(np.array(a))
+
         clamped_control = np.array(a)
         for i in range(len(clamped_control)):
             if clamped_control[i] > self.control_bounds[0][i]:
@@ -53,16 +67,25 @@ class DartHopperEnv(dart_env.DartEnv, utils.EzPickle):
         for contact in contacts:
             total_force_mag += np.square(contact.force).sum()
 
+        joint_limit_penalty = 0
+        for j in range(3, self.robot_skeleton.ndofs):
+            if (self.robot_skeleton.q_lower[j] - self.robot_skeleton.q[j]) > -0.05:
+                joint_limit_penalty += abs(1.0)
+            if (self.robot_skeleton.q_upper[j] - self.robot_skeleton.q[j]) < 0.05:
+                joint_limit_penalty += abs(1.0)
+
         alive_bonus = 1.0
         reward = (posafter - posbefore) / self.dt
         reward += alive_bonus
         reward -= 1e-3 * np.square(a).sum()
-        reward -= 1e-7 * total_force_mag
+        #reward -= 2e-1 * joint_limit_penalty
+        #reward -= 1e-7 * total_force_mag
+
         s = self.state_vector()
         done = not (np.isfinite(s).all() and (np.abs(s[2:]) < 100).all() and
-                    (height > .7) and (abs(ang) < .2))
+                    (height > .7) and (abs(ang) < .4))
         ob = self._get_obs()
-        return ob, reward, done, {'vel_rew':(posafter - posbefore) / self.dt, 'action_rew':1e-3 * np.square(a).sum(), 'forcemag':1e-7*total_force_mag}
+        return ob, reward, done, {'vel_rew':(posafter - posbefore) / self.dt, 'action_rew':1e-3 * np.square(a).sum(), 'forcemag':1e-7*total_force_mag, 'limit_pen':2e-2 * joint_limit_penalty}
 
     def _get_obs(self):
         state =  np.concatenate([
@@ -70,6 +93,22 @@ class DartHopperEnv(dart_env.DartEnv, utils.EzPickle):
             np.clip(self.robot_skeleton.dq,-10,10)
         ])
         state[0] = self.robot_skeleton.bodynodes[2].com()[1]
+
+        if self.use_UPOSI:
+            out_ob = np.zeros(self.OSI_obs_dim)
+            ind = 0
+            for s_a in self.state_action_buffer:
+                out_ob[ind:ind+len(s_a[0])] = np.array(s_a[0])
+                ind += len(s_a[0])
+                out_ob[ind:ind+len(s_a[1])] = np.array(s_a[1])
+                ind += len(s_a[1])
+            out_ob[ind:ind + len(state)] = np.array(state)
+
+            self.state_action_buffer.append([np.array(state)])
+            if len(self.state_action_buffer) > self.history_length:
+                self.state_action_buffer.pop(0)
+
+            return np.array([out_ob], dtype=np.float32)
 
         if self.train_UP:
             state = np.concatenate([state, self.param_manager.get_simulator_parameters()])
@@ -84,6 +123,8 @@ class DartHopperEnv(dart_env.DartEnv, utils.EzPickle):
 
         if self.train_UP:
             self.param_manager.resample_parameters()
+
+        self.state_action_buffer = [] # for UPOSI
 
         return self._get_obs()
 
--- gym/envs/mujoco/hopper.py
+++ gym/envs/mujoco/hopper.py
@@ -4,10 +4,22 @@ from gym.envs.mujoco import mujoco_env
 
 class HopperEnv(mujoco_env.MujocoEnv, utils.EzPickle):
     def __init__(self):
+        # UPOSI variables
+        self.use_UPOSI = False
+        self.history_length = 3 # size of the motion history for UPOSI
+        self.state_action_buffer = []
+
         mujoco_env.MujocoEnv.__init__(self, 'hopper.xml', 4)
         utils.EzPickle.__init__(self)
 
+    def setUseUPOSI(self, useUPOSI = True):
+        self.use_UPOSI = useUPOSI
+        self.OSI_obs_dim = (self.obs_dim+self.act_dim)*self.history_length+self.obs_dim
+
     def _step(self, a):
+        if self.use_UPOSI:
+            self.state_action_buffer[-1].append(np.array(a))
+
         posbefore = self.model.data.qpos[0,0]
         self.do_simulation(a, self.frame_skip)
         posafter,height,ang = self.model.data.qpos[0:3,0]
@@ -23,10 +35,28 @@ class HopperEnv(mujoco_env.MujocoEnv, utils.EzPickle):
         return ob, reward, done, {'vel_rew':(posafter - posbefore) / self.dt, 'action_rew':1e-3 * np.square(a).sum()}
 
     def _get_obs(self):
-        return np.concatenate([
+        state = np.concatenate([
             self.model.data.qpos.flat[1:],
             np.clip(self.model.data.qvel.flat,-10,10)
         ])
+
+        if self.use_UPOSI:
+            out_ob = np.zeros(self.OSI_obs_dim)
+            ind = 0
+            for s_a in self.state_action_buffer:
+                out_ob[ind:ind+len(s_a[0])] = np.array(s_a[0])
+                ind += len(s_a[0])
+                out_ob[ind:ind+len(s_a[1])] = np.array(s_a[1])
+                ind += len(s_a[1])
+            out_ob[ind:ind + len(state)] = np.array(state)
+
+            self.state_action_buffer.append([np.array(state)])
+            if len(self.state_action_buffer) > self.history_length:
+                self.state_action_buffer.pop(0)
+
+            return np.array([out_ob], dtype=np.float32)
+
+        return state
 
     def reset_model(self):
         qpos = self.init_qpos + self.np_random.uniform(low=-.005, high=.005, size=self.model.nq)
