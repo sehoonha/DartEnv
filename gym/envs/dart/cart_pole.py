136fbe601724a25760ab1396cd6dacd6348c0dcf
sehoonha <sehoon.ha@gmail.com>
2017-03-10 15:34:24 -0500

    more interfaces for DartEnv

--- gym/envs/dart/cart_pole.py
+++ gym/envs/dart/cart_pole.py
@@ -4,31 +4,53 @@ from gym.envs.dart import dart_env
 
 class DartCartPoleEnv(dart_env.DartEnv, utils.EzPickle):
     def __init__(self):
-        control_bounds = np.array([[1.0],[-1.0]])
-        self.action_scale = 100
-        dart_env.DartEnv.__init__(self, 'cartpole.skel', 2, 4, control_bounds)
+        self.actions = list()
+        control_bounds = np.array([[10.0],[-10.0]])
+        self.action_scale = 1.0
+        dart_env.DartEnv.__init__(self, 'cartpole.skel', 2, 4, control_bounds,
+                                  dt=0.01)
         utils.EzPickle.__init__(self)
 
     def _step(self, a):
-        reward = 1.0
-
+        # self.actions.append(a)
+        # print("a = %.4f" % a[0])
         tau = np.zeros(self.robot_skeleton.ndofs)
-        tau[0] = a[0] * self.action_scale
+        tau[0] = (a[0] - 0.5) * self.action_scale
 
         self.do_simulation(tau, self.frame_skip)
         ob = self._get_obs()
 
         notdone = np.isfinite(ob).all() and (np.abs(ob[1]) <= .2)
         done = not notdone
+
+        if done:
+            avg = None if len(self.actions) == 0 else np.mean(self.actions)
+            # print("%d actions: avg = %s" % (len(self.actions), avg))
+            self.actions = list()
+
+        # reward = 1.0
+        notdone = 1 - int(done)
+        ucost = 1e-5 * (a ** 2).sum()
+        xcost = 1 - np.cos(ob[1])
+        # xcost2 = 1e-2 * (ob[0] ** 2)
+        reward = notdone * 10 - notdone * (xcost) - notdone * ucost
+        # print(ob)
+
         return ob, reward, done, {}
 
+    def log_diagnostics(self, paths):
+        pass
+
+    def terminate(self, ):
+        pass
 
     def _get_obs(self):
         return np.concatenate([self.robot_skeleton.q, self.robot_skeleton.dq]).ravel()
 
     def reset_model(self):
+        self.actions = list()
         self.dart_world.reset()
-        qpos = self.robot_skeleton.q + self.np_random.uniform(low=-.01, high=.01, size=self.robot_skeleton.ndofs)
+        qpos = self.robot_skeleton.q + self.np_random.uniform(low=-.05, high=.05, size=self.robot_skeleton.ndofs)
         qvel = self.robot_skeleton.dq + self.np_random.uniform(low=-.01, high=.01, size=self.robot_skeleton.ndofs)
         self.set_state(qpos, qvel)
         return self._get_obs()
--- gym/envs/dart/dart_env.py
+++ gym/envs/dart/dart_env.py
@@ -1,6 +1,7 @@
 import os
 
-from gym import error, spaces
+from gym import error  # , spaces
+from rllab import spaces
 from gym.utils import seeding
 import numpy as np
 from os import path
@@ -25,19 +26,14 @@ class DartEnv(gym.Env):
     def __init__(self, model_path, frame_skip, observation_size, action_bounds, dt=0.002):
         print('pydart initialization OK')
 
-        if model_path.startswith("/"):
-            fullpath = model_path
-        else:
-            fullpath = os.path.join(os.path.dirname(__file__), "assets", model_path)
-        if not path.exists(fullpath):
-            raise IOError("File %s does not exist"%fullpath)
-
-        self.dart_world = pydart.World(dt, fullpath)
+        self.dart_world = self.create_world(model_path)
         self.robot_skeleton = self.dart_world.skeletons[-1] # assume that the skeleton of interest is always the last one
 
         for jt in range(0, len(self.robot_skeleton.joints)):
             if self.robot_skeleton.joints[jt].has_position_limit(0):
                 self.robot_skeleton.joints[jt].set_position_limit_enforced(True)
+        for dof in self.robot_skeleton.dofs:
+            dof.set_damping_coefficient(0.05)
 
         self.frame_skip= frame_skip
         self.viewer = None
@@ -71,7 +67,16 @@ class DartEnv(gym.Env):
             'video.frames_per_second' : int(np.round(1.0 / self.dt))
         }
 
+    def create_world(self, model_path):
+        if model_path.startswith("/"):
+            fullpath = model_path
+        else:
+            fullpath = os.path.join(os.path.dirname(__file__), "assets", model_path)
+        if not path.exists(fullpath):
+            raise IOError("File %s does not exist"%fullpath)
 
+        dart_world = pydart.World(dt, fullpath)
+        return dart_world
 
     def _seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
